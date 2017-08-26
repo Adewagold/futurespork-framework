349eaa44cfb7f350a454f061989985363bb0ff11
Adewale Adeleye <Adewagold@yahoo.com>
2017-08-26 03:40:02 +0100

    base model created

--- classes/Model.php
+++ classes/Model.php
@@ -7,7 +7,16 @@ abstract class Model
 
 	public function __construct()
 	{
-		$this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";dbuser=".DB_USER,PASSWORD);
+		try {
+		$options = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE 	=> PDO::ERRMODE_EXCEPTION
+			);
+		$this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";dbuser=".DB_USER,PASSWORD, $options);
+			//echo "successful";
+		} catch (Exception $e) {
+			$this->error = $e->getMessage();
+		}
 	}
 } 
 
