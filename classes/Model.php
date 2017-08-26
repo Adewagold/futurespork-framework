d862293b96a3c95fb6630d160306176638bc9887
Adewale Adeleye <Adewagold@yahoo.com>
2017-08-26 04:08:13 +0100

    Setup Model

--- classes/Model.php
+++ classes/Model.php
@@ -18,6 +18,49 @@ abstract class Model
 			$this->error = $e->getMessage();
 		}
 	}
+
+	public function query($query)
+	{
+		$this->statement = $this->dbh->prepare($query);
+	}
+
+	public function bind($param, $value, $type=null)
+	{
+		if(is_null($type))
+		{
+			switch (true) {
+				case is_int($value):
+					$type= PDO::PARAM_INT;
+					break;
+				case is_bool($value):
+					$type= PDO::PARAM_BOOL;
+					break;
+				case is_null($value):
+					$type= PDO::PARAM_NULL;
+					break;
+				default:
+					$type=PDO::PARAM_STR;
+					break;
+			}
+		}
+		$this->statement->bindValue($param, $value, $type);
+	}
+ 
+ 	
+	public function execute()
+	{
+		return $this->statement->execute();
+	}
+
+	public function resultSet(){
+		$this->execute(); 
+		return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function lastInsertId()
+	{
+		$this->dbh->lastInsertId();  
+	}
 } 
 
 ?>
