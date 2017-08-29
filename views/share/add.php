4df91e0c94d6cc7b085fb372f4103159b76241ae
Adewale Adeleye <Adewagold@yahoo.com>
2017-08-29 13:01:22 +0100

    View Loaded into shared index

--- views/share/add.php
+++ views/share/add.php
@@ -3,7 +3,8 @@
         <p class="lead text-center">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 	<div class="row">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-		<button type="button" class="btn btn-success">Add Shares</button>
+
+		<a class="btn btn-primary" href="<?php echo ROOT_URL;?>share/add" role="button">Add Shares</a>
 	</div>
 </div>
 </div>
--- views/share/index.php
+++ views/share/index.php
@@ -1,4 +1,20 @@
 <div class="starter-template">
         <h1>Shared Page</h1>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+        	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+
+		<a class="btn btn-primary" href="<?php echo ROOT_URL;?>share/add" role="button">Add Shares</a>
+
+		
+		 
+	</div>
+
       </div>
+      <?php
+			foreach ($viewModel as $item) : ?>
+				<div class="well">
+					<h3><?php echo $item['title'] ?></h3>
+					<p><?php echo $item['body'] ?></p>
+				</div>
+		<?php endforeach ?>
