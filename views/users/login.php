cf65874e136b5b645cfd374793ddc44ae480968d
Adewale Adeleye <Adewagold@yahoo.com>
2017-09-24 15:36:03 +0100

    User Login form created

---
+++ views/users/login.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+	
+	<form method="post" action="/login">
+		<label> Username</label>
+		<input type="text" placeholder="Username" class="form-control" name="username">
+
+		<label> Password</label>
+		<input type="text" placeholder="Username" class="form-control" password="password">
+
+		<input type="submit" class="btn btn-primary">
+	</form>
+
+</div>
